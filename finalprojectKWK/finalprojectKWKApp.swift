b763a5943de13aba122cd71e583020f6c55cb656
kennedy miller <kennedymiller@ca.rr.com>
2023-06-12 08:25:29 -0700

    Initial Commit

---
+++ finalprojectKWK/finalprojectKWKApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalprojectKWKApp.swift
+//  finalprojectKWK
+//
+//  Created by Kennedy Miller on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct finalprojectKWKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

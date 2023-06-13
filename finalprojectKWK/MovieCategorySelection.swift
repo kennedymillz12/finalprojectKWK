620f24de195c2f665529e7cbe9b65ec3c4c7ef5f
Gianna Buscho <giannabuscho@gmail.com>
2023-06-13 12:12:28 -0700

    AddingUIFiles

---
+++ finalprojectKWK/MovieCategorySelection.swift
@@ -0,0 +1,20 @@
+//
+//  MovieCategorySelection.swift
+//  finalprojectKWK
+//
+//  Created by scholar on 6/13/23.
+//
+
+import SwiftUI
+
+struct MovieCategorySelection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieCategorySelection_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieCategorySelection()
+    }
+}
---
+++ finalprojectKWK/SnackCategorySelection.swift
@@ -0,0 +1,20 @@
+//
+//  SnackCategorySelection.swift
+//  finalprojectKWK
+//
+//  Created by scholar on 6/13/23.
+//
+
+import SwiftUI
+
+struct SnackCategorySelection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SnackCategorySelection_Previews: PreviewProvider {
+    static var previews: some View {
+        SnackCategorySelection()
+    }
+}

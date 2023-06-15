b5673efb170bb6e519b0538870e1ddba48138665
Selina <crystalicyrose@gmail.com>
2023-06-15 09:44:10 -0700

    movie display

--- finalprojectKWK/MovieDisplay.swift
+++ finalprojectKWK/MovieDisplay.swift
@@ -9,19 +9,35 @@ import SwiftUI
 
 struct MovieDisplay: View {
     @State var image = "IceSpice"
-    @State var review = "Coral"
+    @State var review = "Review"
+    @State var movie = ""
     
     var body: some View {
-        VStack{
-            Text(review)
-                .padding()
+        ZStack {
             
-                .padding()
-            
-            Image(image)
-                .resizable(resizingMode: .stretch)
-                .aspectRatio(contentMode: .fit)
-            
+            Color(red: 0.097, green: 0.106, blue: 0.14)
+                .ignoresSafeArea()
+            VStack{
+                Text("You should watch...")
+                    .font(Font.custom("Limelight-Regular", size: 30))
+                    .foregroundColor(Color(red: 0.792156862745098, green: 0.5254901960784314, blue: 0.10196078431372549))
+                
+                
+                Image(image)
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                    .cornerRadius(15)
+                Text(review)
+                    .font(Font.custom("Limelight-Regular", size: 20))
+                
+                    .foregroundColor(Color(red: 1.0, green: 0.996078431372549, blue: 0.6784313725490196))
+                    .padding()
+                
+                    .padding()
+                
+                
+            }
         }
     }
 }
